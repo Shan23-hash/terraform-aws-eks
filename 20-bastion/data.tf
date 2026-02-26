b97d9164eabd4ac1f3738309f7f6020036d267fc
shanthi <shanbojja@gmail.com>
2026-02-27 00:13:48 +0530

    jenkins

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -1,10 +1,10 @@
 data "aws_ami" "joindevops" {
-  owners           = ["self"]
+  owners           = ["973714476881"]
   most_recent      = true
 
   filter {
     name   = "name"
-    values = ["RHEL-9-DevOps-Practice*"]
+    values = ["RHEL-9-DevOps-Practice"]
   }
 
   filter {
